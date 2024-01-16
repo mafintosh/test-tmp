838df399895fee515d6ca1263c113d81d5e380ec
Dan <ruy-dan@protonmail.com>
2024-01-16 15:46:32 +0100

    Optional directory name (#1)
    * add optional dir name
    
    * fsp realpath in test

--- test.js
+++ test.js
@@ -1,9 +1,38 @@
 const test = require('brittle')
 const tmp = require('./')
 const fs = require('fs')
+const os = require('os')
+const path = require('path')
 
 test('basic', async function (t) {
   const dir = await tmp(t)
 
   t.alike(await fs.promises.readdir(dir), [])
+})
+
+test('specified name', async function (t) {
+  const name = 'testdir'
+  const dir = await tmp(t, name)
+  t.ok(dir.includes(name), 'directory contains specified name')
+  t.alike(await fs.promises.readdir(dir), [])
+})
+
+test('invalid directory name', async function (t) {
+  const name = '<>:/\\|?*'
+  try {
+    await tmp(t, name)
+    t.pass('should default to a random name when an invalid input is provided')
+  } catch (error) {
+    t.fail('expected to handle an invalid directory name')
+  }
+})
+
+test('reuse directory', async function (t) {
+  const name = 'existing-dir'
+  const existing = path.join(await fs.promises.realpath(os.tmpdir()), 'tmp-test-' + name)
+
+  await fs.promises.mkdir(existing, { recursive: true })
+
+  const dir = await tmp(t, name)
+  t.is(dir, existing, 'uses the existing directory when it already exists')
 })
